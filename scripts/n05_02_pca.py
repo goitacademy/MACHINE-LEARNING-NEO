b9e1ebce3af5dc4e5a2464089069036e8d760b33
Volodymyr Holomb <wldmrgml@gmail.com>
2024-03-18 16:11:25 +0200

    add feature importance plot

--- scripts/n05_02_pca.py
+++ scripts/n05_02_pca.py
@@ -1,13 +1,14 @@
-from scipy.stats import zscore
-from sklearn.metrics import accuracy_score
-from sklearn.ensemble import GradientBoostingClassifier
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy.stats import zscore
 from sklearn.decomposition import PCA
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import GradientBoostingClassifier
 
 # %%
 
@@ -17,6 +18,10 @@ data.head()
 # %%
 
 data.info()
+
+# %%
+
+target.value_counts()
 
 # %%
 
@@ -132,3 +137,16 @@ pred_pca = clf_pca.predict(X_test_pca.iloc[:, :n_components])
 score_pca = accuracy_score(y_test, pred_pca)
 
 print(f'Model accuracy (PCA): {score_pca:.1%}')
+
+# %%
+
+plt.figure(figsize=(3, 8))
+
+(pd.Series(
+    data=clf_full.feature_importances_,
+    index=X_train.columns)
+    .sort_values(ascending=True)
+    .plot
+    .barh())
+
+plt.show()
